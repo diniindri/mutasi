af6861b8cda8fe5bd01a2bd1bdba13feffb98dda
danakristiawan <danakristiawan@gmail.com>
2021-07-07 19:50:07 +0700

    the next commit

--- application/views/biaya_mutasi/detail.php
+++ application/views/biaya_mutasi/detail.php
@@ -1,6 +1,16 @@
 <main class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Biaya Mutasi</h1>
+        <h1 class="h2">Detail</h1>
+    </div>
+    <div class="row">
+        <div class="col">
+            <?php if ($this->session->flashdata('pesan')) : ?>
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Selamat!</strong> <?= $this->session->flashdata('pesan'); ?>
+                    <button type="button" class="btn-close" data-dismiss="alert" aria-label="Close"></button>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
     <div class="row mb-3">
         <div class="col-lg-7">
@@ -8,7 +18,7 @@
         <div class="col-lg-5">
             <form action="" method="post" autocomplete="off">
                 <div class="input-group">
-                    <input type="text" name="nama" class="form-control" placeholder="uraian sk">
+                    <input type="text" name="nmpeg" class="form-control" placeholder="nama pegawai">
                     <button class="btn btn-sm btn-outline-secondary" type="submit">Cari</button>
                 </div>
             </form>
@@ -21,25 +31,21 @@
                     <thead class="text-center">
                         <tr class="align-middle">
                             <th>No</th>
-                            <th>Nomor</th>
-                            <th>Tanggal</th>
-                            <th>Uraian</th>
-                            <th>Status</th>
+                            <th>NIP</th>
+                            <th>Nama</th>
                             <th>Aksi</th>
                         </tr>
                     </thead>
                     <tbody>
                         <?php $no = $page + 1;
-                        foreach ($sk as $r) : ?>
+                        foreach ($pegawai as $r) : ?>
                             <tr>
                                 <td class="text-center"><?= $no++; ?></td>
-                                <td><?= $r['nomor']; ?></td>
-                                <td><?= date('d-m-Y', $r['tanggal']); ?></td>
-                                <td><?= $r['uraian']; ?></td>
-                                <td><?= $r['status'] == 1 ? 'Aktif' : 'Non Aktif'; ?></td>
-                                <td class="pb-0">
+                                <td><?= $r['nip']; ?></td>
+                                <td><?= $r['nmpeg']; ?></td>
+                                <td class="pb-0 pr-0">
                                     <div class="btn-group btn-group-sm" role="group">
-                                        <a href="<?= base_url('biaya-mutasi/detail/') . $r['id']; ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0">Detail</a>
+                                        <a href="<?= base_url('pegawai/hitung/') . $r['id'] . '/' . $sk_id; ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0">Hitung</a>
                                     </div>
                                 </td>
                             </tr>
@@ -51,7 +57,8 @@
     </div>
     <div class="row">
         <div class="col-lg-6">
-            <?= $uraian == null ? $pagination : ''; ?>
+            <?= $nmpeg == null ? $pagination : ''; ?>
         </div>
     </div>
+
 </main>
