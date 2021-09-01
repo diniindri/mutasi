83e42ac0f880ad4f427d639f06c6963cd454db0d
Alam Firdaus <alamfirdaus@Alams-MacBook-Pro.local>
2021-09-01 15:48:22 +0700

    perubahan timeline dan dasboard

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -2,40 +2,166 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Dashboard</h1>
     </div>
-    <div class="row">
+    <div class="row mb-3">
         <div class="col">
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover">
-                    <thead class="text-center">
-                        <tr class="align-middle">
-                            <th>#</th>
-                            <th>Nomor</th>
-                            <th>Uraian</th>
-                            <th>Tanggal</th>
-                            <th>Aksi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                        <?php $no = 1;
-                        foreach ($sk as $r) : ?>
-
-                            <tr>
-                                <td class="text-center"><?= $no++; ?></td>
-                                <td><?= $r['nomor']; ?></td>
-                                <td><?= $r['uraian']; ?></td>
-                                <td><?= $r['tanggal']; ?></td>
-                                <td class="pb-0 pr-0">
-                                    <div class="btn-group btn-group-sm" role="group">
-                                        <a href="<?= base_url('dashboard/detail'); ?>" class="btn btn-sm btn-outline-secondary pt-0 pb-0">Detail</a>
-                                    </div>
-                                </td>
-                            </tr>
-
-                        <?php endforeach; ?>
-
-                    </tbody>
-                </table>
+            <a href="" class="btn btn-sm btn-outline-secondary mt-1 mb-1">KEP-9/KN/UP.11/2021</a>
+            <a href="" class="btn btn-sm btn-outline-secondary mt-1 mb-1">KEP-56/KN/UP.11/2020</a>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col">
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="text-center">
+                                <tr class="align-middle">
+                                    <th>Jenis</th>
+                                    <th>Uraian</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Tanggal SK</td>
+                                    <td>02 Januari 2021</td>
+                                </tr>
+                                <tr>
+                                    <td>Uraian SK</td>
+                                    <td>Pembayaran Biaya Mutasi Gol II dan Gol III sesuai SK Kep-09/KN.1/2021</td>
+                                </tr>
+                                <tr>
+                                    <td>Rute Mutasi</td>
+                                    <td>Jakarta - Bogor</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="card">
+                <div class="card-header">
+                    Featured
+                </div>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">2</th>
+                                    <td>Jacob</td>
+                                    <td>Thornton</td>
+                                    <td>@fat</td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">3</th>
+                                    <td colspan="2">Larry the Bird</td>
+                                    <td>@twitter</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
--- application/views/timeline/index.php
+++ application/views/timeline/index.php
@@ -4,97 +4,107 @@
     </div>
     <div class="row">
         <div class="col-lg-3">
-            <div class="list-group">
-                <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2 active" aria-current="true">
-                    KEP-9/KN/UP.11/2021
-                </a>
-                <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2">KEP-56/KN/UP.11/2020</a>
-                <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2">KEP-14/KN/UP.11/2019</a>
+            <div class="card">
+                <h5 class="card-header">Daftar SK</h5>
+                <div class="card-body">
+                    <div class="list-group">
+                        <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2 active" aria-current="true">
+                            KEP-9/KN/UP.11/2021
+                        </a>
+                        <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2">KEP-56/KN/UP.11/2020</a>
+                        <a href="#" class="btn btn-sm btn-outline-secondary mb-2 text-left p-2">KEP-14/KN/UP.11/2019</a>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="col-lg-9">
-            <div class="table-responsive">
-                <table class="table table-bordered table-hover">
-                    <thead class="text-center">
-                        <tr class="align-middle">
-                            <th>No</th>
-                            <th>Uraian</th>
-                            <th>Keterangan</th>
-                            <th>Tanggal</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Penerbitan SK Mutasi</td>
-                            <td>Nomor KEP-9/KN/UP.11/2021 Tentang Pemindahan Pegawai Negeri Sipil di Lingkungan DJKN</td>
-                            <td>2 September 2021</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Upload KP4</td>
-                            <td>Sudah upload</td>
-                            <td>4 September 2021</td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Verifikasi Data Keluarga oleh Kantor Pusat</td>
-                            <td> - </td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Perhitungan Biaya Mutasi oleh Kantor Pusat</td>
-                            <td>Belum upload</td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>Upload Rincian Biaya Mutasi</td>
-                            <td>Belum upload</td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>Upload SPD Kantor Asal</td>
-                            <td>Belum upload</td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>7</td>
-                            <td>Upload SPD Kantor Tujuan</td>
-                            <td>Belum upload</td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>8</td>
-                            <td>Verifikasi Rincian Biaya Mutasi dan SPD</td>
-                            <td> - </td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>9</td>
-                            <td>Verifikasi Rekening Pegawai dan Pembuatan Data Payroll</td>
-                            <td> - </td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>10</td>
-                            <td>Payroll Biaya Mutasi</td>
-                            <td> - </td>
-                            <td> - </td>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div class="card">
+                <h5 class="card-header">Status Pembayaran</h5>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead class="text-center">
+                                <tr class="align-middle">
+                                    <th>No</th>
+                                    <th>Uraian</th>
+                                    <th>Keterangan</th>
+                                    <th>Tanggal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Penerbitan SK Mutasi</td>
+                                    <td>Nomor KEP-9/KN/UP.11/2021 Tentang Pemindahan Pegawai Negeri Sipil di Lingkungan DJKN</td>
+                                    <td>2 September 2021</td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Upload KP4</td>
+                                    <td>Sudah upload</td>
+                                    <td>4 September 2021</td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>3</td>
+                                    <td>Verifikasi Data Keluarga oleh Kantor Pusat</td>
+                                    <td> - </td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>4</td>
+                                    <td>Perhitungan Biaya Mutasi oleh Kantor Pusat</td>
+                                    <td>Belum upload</td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>5</td>
+                                    <td>Upload Rincian Biaya Mutasi</td>
+                                    <td>Belum upload</td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>6</td>
+                                    <td>Upload SPD Kantor Asal</td>
+                                    <td>Belum upload</td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>7</td>
+                                    <td>Upload SPD Kantor Tujuan</td>
+                                    <td>Belum upload</td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>Verifikasi Rincian Biaya Mutasi dan SPD</td>
+                                    <td> - </td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>9</td>
+                                    <td>Verifikasi Rekening Pegawai dan Pembuatan Data Payroll</td>
+                                    <td> - </td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>10</td>
+                                    <td>Payroll Biaya Mutasi</td>
+                                    <td> - </td>
+                                    <td> - </td>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
