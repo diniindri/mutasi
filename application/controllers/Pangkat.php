1823a6ef381e7b1b46be1da7570d256133697cef
Alam Firdaus <alamfirdaus@Alams-MacBook-Pro.local>
2021-10-05 14:46:16 +0700

    referensi laporan n pangkat

--- application/controllers/Pangkat.php
+++ application/controllers/Pangkat.php
@@ -1,67 +1,70 @@
 <?php
 
-class Laporan extends CI_Controller
+use Spipu\Html2Pdf\Html2Pdf;
+
+class Pangkat extends CI_Controller
 {
     public function __construct()
     {
         parent::__construct();
         is_logged_in();
-        $this->load->model('Ref_laporan_model', 'laporan');
+        // meload file ref_pangkat_model.php
+        $this->load->model('Ref_pangkat_model', 'pangkat');
     }
 
     public function index()
     {
-        // menangkap data pencarian nama laporan
-        $nama = $this->input->post('nama');
+        // menangkap data pencarian nama pangkat
+        $kdgol = $this->input->post('kdgol');
 
         // settingan halaman
-        $config['base_url'] = base_url('laporan/index');
-        $config['total_rows'] = $this->laporan->countLaporan();
+        $config['base_url'] = base_url('pangkat/index');
+        $config['total_rows'] = $this->pangkat->countPangkat();
         $config['per_page'] = 10;
         $config["num_links"] = 3;
         $this->pagination->initialize($config);
         $data['pagination'] = $this->pagination->create_links();
         $data['page'] = $this->uri->segment(3) ? $this->uri->segment(3) : 0;
-        $data['nama'] = $nama;
+        $data['kdgol'] = $kdgol;
         $limit = $config["per_page"];
         $offset = $data['page'];
 
-        // pilih tampilan data, semua atau berdasarkan pencarian nama laporan
-        if ($nama) {
+        // pilih tampilan data, semua atau berdasarkan pencarian kode golongan
+        if ($kdgol) {
             $data['page'] = 0;
             $offset = 0;
-            $data['laporan'] = $this->laporan->findLaporan($nama, $limit, $offset);
+            $data['pangkat'] = $this->pangkat->findPangkat($kdgol, $limit, $offset);
         } else {
-            $data['laporan'] = $this->laporan->getLaporan($limit, $offset);
+            $data['pangkat'] = $this->pangkat->getPangkat($limit, $offset);
         }
 
-        // meload view pada laporan/index.php
+        // meload view pada pangkat/index.php
         $this->load->view('template/header');
         $this->load->view('template/sidebar');
-        $this->load->view('laporan/index', $data);
+        $this->load->view('pangkat/index', $data);
         $this->load->view('template/footer');
     }
 
     // validasi inputan pada form
     private $rules = [
         [
-            'field' => 'tahun_anggaran',
-            'label' => 'Tahun',
-            'rules' => 'required|trim|exact_length[4]'
-        ],
-        [
-            'field' => 'nomor_spd',
-            'label' => 'SPD',
+            'field' => 'kdgol',
+            'label' => 'Kdgol',
             'rules' => 'required|trim'
         ],
         [
-            'field' => 'akun',
-            'label' => 'Akun',
+            'field' => 'nmgol',
+            'label' => 'Nmgol',
             'rules' => 'required|trim'
         ],
         [
-            'field' => 'dipa_kantor',
-            'label' => 'DIPA',
+            'field' => 'kdgapok',
+            'label' => 'Kdgapok',
+            'rules' => 'required|trim'
+        ],
+        [
+            'field' => 'nama',
+            'label' => 'nama',
             'rules' => 'required|trim'
         ],
     ];
@@ -73,21 +76,21 @@ class Laporan extends CI_Controller
         // jika validasi sukses
         if ($validation->run()) {
             $data = [
-                'tahun_anggaran' => htmlspecialchars($this->input->post('tahun_anggaran', true)),
-                'nomor_spd' => htmlspecialchars($this->input->post('nomor_spd', true)),
-                'akun' => htmlspecialchars($this->input->post('akun', true)),
-                'dipa_kantor' => htmlspecialchars($this->input->post('dipa_kantor', true))
+                'kdgol' => htmlspecialchars($this->input->post('kdgol', true)),
+                'nmgol' => htmlspecialchars($this->input->post('nmgol', true)),
+                'kdgapok' => htmlspecialchars($this->input->post('kdgapok', true)),
+                'nama' => htmlspecialchars($this->input->post('nama', true))
             ];
             // simpan data ke database melalui model
-            $this->laporan->createLaporan($data);
+            $this->pangkat->createPangkat($data);
             $this->session->set_flashdata('pesan', 'Data berhasil ditambah.');
-            redirect('laporan');
+            redirect('pangkat');
         }
 
-        // meload view pada laporan/create.php
+        // meload view pada pangkat/create.php
         $this->load->view('template/header');
         $this->load->view('template/sidebar');
-        $this->load->view('laporan/create');
+        $this->load->view('pangkat/create');
         $this->load->view('template/footer');
     }
 
@@ -97,28 +100,28 @@ class Laporan extends CI_Controller
         if (!isset($id)) show_404();
 
         // load data dokumen yang akan diubah
-        $data['laporan'] = $this->laporan->getDetailLaporan($id);
+        $data['pangkat'] = $this->pangkat->getDetailPangkat($id);
 
         $validation = $this->form_validation->set_rules($this->rules);
 
         // jika validasi sukses
         if ($validation->run()) {
             $data = [
-                'tahun_anggaran' => htmlspecialchars($this->input->post('tahun_anggaran', true)),
-                'nomor_spd' => htmlspecialchars($this->input->post('nomor_spd', true)),
-                'akun' => htmlspecialchars($this->input->post('akun', true)),
-                'dipa_kantor' => htmlspecialchars($this->input->post('dipa_kantor', true))
+                'kdgol' => htmlspecialchars($this->input->post('kdgol', true)),
+                'nmgol' => htmlspecialchars($this->input->post('nmgol', true)),
+                'kdgapok' => htmlspecialchars($this->input->post('kdgapok', true)),
+                'nama' => htmlspecialchars($this->input->post('nama', true))
             ];
             // update data di database melalui model
-            $this->laporan->updateLaporan($data, $id);
+            $this->pangkat->updatePangkat($data, $id);
             $this->session->set_flashdata('pesan', 'Data berhasil diubah.');
-            redirect('laporan');
+            redirect('pangkat');
         }
 
-        // meload view pada dokumen/update.php
+        // meload view pada pangkat/update.php
         $this->load->view('template/header');
         $this->load->view('template/sidebar');
-        $this->load->view('laporan/update', $data);
+        $this->load->view('pangkat/update', $data);
         $this->load->view('template/footer');
     }
 
@@ -128,9 +131,9 @@ class Laporan extends CI_Controller
         if (!isset($id)) show_404();
 
         // hapus data di database melalui model
-        if ($this->laporan->deleteLaporan($id)) {
+        if ($this->pangkat->deletePangkat($id)) {
             $this->session->set_flashdata('pesan', 'Data berhasil dihapus.');
         }
-        redirect('laporan');
+        redirect('pangkat');
     }
 }
---
+++ application/models/Ref_pangkat_model.php
@@ -0,0 +1,55 @@
+<?php
+
+class Ref_pangkat_model extends CI_Model
+{
+    public function getPangkat($limit = 0, $offset = 0)
+    {
+        $this->db->limit($limit, $offset);
+        return $this->db->get('ref_pangkat')->result_array();
+    }
+
+    public function getDetailPangkat($id = null)
+    {
+        return $this->db->get_where('ref_pangkat', ['id' => $id])->row_array();
+    }
+
+    public function findPangkat($kdgol = null, $limit = 0, $offset = 0)
+    {
+        $this->db->limit($limit, $offset);
+        return $this->db->get_where('ref_pangkat', ['kdgol' => $kdgol])->result_array();
+    }
+
+    public function countPangkat()
+    {
+        return $this->db->get('ref_pangkat')->num_rows();
+    }
+
+    public function createPangkat($data = null)
+    {
+        $this->db->insert('ref_pangkat', $data);
+        return $this->db->affected_rows();
+    }
+
+    public function updatePangkat($data = null, $id = null)
+    {
+        $this->db->update('ref_pangkat', $data, ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function deletePangkat($id = null)
+    {
+        $this->db->delete('ref_pangkat', ['id' => $id]);
+        return $this->db->affected_rows();
+    }
+
+    public function getAllPangkat()
+    {
+        $this->db->order_by('nama', 'ASC');
+        return $this->db->get('ref_pangkat')->result_array();
+    }
+
+    public function getKodePangkat($kode = null)
+    {
+        return $this->db->get_where('ref_pangkat', ['kode' => $kode])->row_array();
+    }
+}
